f4c5da19e7672436725d94e4402d12b4a9769ba7
Nitin Singh <nitinsingh717@gmail.com>
2020-12-24 14:28:59 +0530

    solved-6.3
    MultipleArbitraryPrecisionIntegers solved and all the test cases are passing now.

--- arrays/src/main/java/MultipleArbitraryPrecisionIntegers.java
+++ arrays/src/main/java/MultipleArbitraryPrecisionIntegers.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
@@ -8,7 +10,41 @@ public class MultipleArbitraryPrecisionIntegers {
     */
 
     public static List<Integer> multiply(List<Integer> a, List<Integer> b) {
-
-        return Collections.emptyList();
+    	int sign = (a.get(0) ^ b.get(0)) < 0 ? -1 : 1;
+    	List<Integer> result = new ArrayList<>(Collections.nCopies(a.size() + b.size(), 0));
+    	
+    	for (int i = a.size()-1; i >=0 ; i--) {
+			for (int j = b.size()-1; j >=0; j--) {
+				
+				int temp = result.get(i+j+1) + (a.get(i)*b.get(j));
+				result.set(i+j+1, temp);
+				result.set(i+j, result.get(i+j) + temp/10);
+				result.set(i+j+1, temp%10);
+			}
+		}
+    	
+    	result = removeFrontZeros(result);
+    	
+    	if(result.isEmpty()) {
+    		return Arrays.asList(0);
+    	}
+    	
+    	result.set(0, result.get(0) * sign);
+        return result;
     }
+    
+    private static List<Integer> removeFrontZeros(List<Integer> result) {
+    	int index = 0;
+		while(index < result.size() && result.get(index) == 0) {
+			index++;
+		}
+		return result.subList(index, result.size());
+	}
+
+	public static void main(String[] args) {
+		List<Integer> l = multiply(Arrays.asList(0), Arrays.asList(1,2));
+		
+		l.forEach(System.out::print);
+	}
+    
 }
